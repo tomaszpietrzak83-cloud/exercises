9d8deea11b2a81f4fea8bd56381be5a2f9f2ecdf
Tomasz Pietrzak <tomaszpietrzak83@wp.pl>
2026-04-16 09:52:40 +0200

    fix: changed "== False" -> not

--- 01_sort01.py
+++ 01_sort01.py
@@ -1,30 +1,33 @@
-#Sort letters of string in alphabetical order
+# Sort letters of string in alphabetical order
 phrase = "Something is Wrong with this string"
+
 
 def sort(text):
 
     characters = list(text)
-        
+
     characters.sort()
-        
-    sorted_string = ''.join(characters)
-    if ' ' in sorted_string:
+
+    sorted_string = "".join(characters)
+    if " " in sorted_string:
         response = input("Do you want to remove spaces? (yes/no): ")
 
         if response.lower() == "yes" or "y":
-            sorted_string = sorted_string.replace(' ', '')
+            sorted_string = sorted_string.replace(" ", "")
 
-    if sorted_string.islower() == False:
+    if not sorted_string.islower():
         response = input("Do you want to convert all letters to lowercase? (yes/no): ")
 
         if response.lower() == "yes" or "y":
             sorted_string = sorted_string.lower()
             letters = list(sorted_string)
             letters.sort()
-            sorted_string = ''.join(letters)
+            sorted_string = "".join(letters)
 
         else:
             return sorted_string
-    
+
     return sorted_string
+
+
 print(sort(phrase))
